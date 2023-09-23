7afa16c870af70e86493257afa6fc1f6f2c0fac4
TayronCazares <144195160+TayronCazares@users.noreply.github.com>
2023-09-23 15:06:52 -0600

    Add files via upload

---
+++ Python/Potencias.py
@@ -0,0 +1,7 @@
+#Obtenemos los datos del usuario y los guardamos en variables
+numero = int(input("Inserte un numero --> "))
+potencia = int(input("Ingrese a que potencia desea elevar el numero --> "))
+#En la variable resultado declaramos la operacion de potencia
+resultado = numero**potencia
+#Mostramos en pantalla el resultado
+print("Tu numero a la potencia ",potencia," es ",resultado)
